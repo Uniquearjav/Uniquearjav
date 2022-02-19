dde2218603ca773deb21ea7b6dcee86d80246e68
Uniquearjav <92720814+Uniquearjav@users.noreply.github.com>
2022-02-19 18:48:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function myMoveFunction() { 
+document.getElementById("nav1").style.background = "#95ccff";
+
+document.getElementById("nav1").style.colour = "#ffffff";
+}
+
+function mouseLeave() {
+  document.getElementById("nav1").style.color = "black";
+  
+document.getElementById("nav1").style.background = "#ffffff";
+  }
+
